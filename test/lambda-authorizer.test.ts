6aa94e2af52e7fae33c8675b80b7a390df5b4589
RAO FAHAD <raofahad046@gmail.com>
2023-11-13 04:13:25 +0500

    Initial commit

---
+++ test/lambda-authorizer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaAuthorizer from '../lib/lambda-authorizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-authorizer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaAuthorizer.LambdaAuthorizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
