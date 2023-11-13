62841266c8b692926d774621f17d05e1598d2350
RAO FAHAD <raofahad046@gmail.com>
2023-11-13 12:01:16 +0500

    created 3 lambda with lambdaAuthroizer, createUserLambda and getUserLambda

--- lib/lambda-authorizer-stack.ts
+++ lib/lambda-authorizer-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 import * as apigwv2_integrations from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class LambdaAuthorizerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -27,6 +28,36 @@ export class LambdaAuthorizerStack extends cdk.Stack {
         allowMethods: [apigwv2.CorsHttpMethod.POST],
         allowCredentials: false,
         allowOrigins: ["*"],
+      },
+    });
+
+    const lambdaAuthorizor = new lambda.Function(this, `${service}-${stage}-lambda`, {
+      functionName: `${service}-${stage}-lambda`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "lambdaAuthroizer.handler",
+      environment: {
+        TABLE_NAME: userTable.tableName,
+      },
+    });
+
+    const createUserLambda = new lambda.Function(this, `${service}-${stage}-create-users-lambda`, {
+      functionName: `${service}-${stage}-create-users-lambda`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "CreateUsers.handler",
+      environment: {
+        TABLE_NAME: userTable.tableName,
+      },
+    });
+
+    const getUsersLambda = new lambda.Function(this, `${service}-${stage}-get-users-lambda`, {
+      functionName: `${service}-${stage}-get-users-lambda`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "GetUsers.handler",
+      environment: {
+        TABLE_NAME: userTable.tableName,
       },
     });
   }
